b2503a970c982358ac35a49811bb3e1de9c04be1
Souvik273 <souvikgoswami464@gmail.com>
2025-01-24 13:14:32 +0530

    auth middleware for authentication updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,18 +1,20 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const auth = async(req,res,next)=>{
-    try {
-        const token = req.headers.authorizarion?.split("")[1]
-        if(!token){
-            res.status(400).json({message:`please login first`})
-        }
-        const decoded = jwt.verify(token,process.env.SECRET_KEY)
-        req.body.userId = decoded.userId
-        req.body.user = decoded.user
-        next()
-    } catch (error) {
-        res.status(500).send(error.message)
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1]; 
+    if (!token) {
+      return res.status(401).json({ message: 'Please login first' });
     }
-}
 
-module.exports= auth
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(`Decoded Token: ${JSON.stringify(decoded)}`);
+    req.body.userId = decoded.userId;
+    req.body.username = decoded.username;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Authentication failed', error: error.message });
+  }
+};
+
+module.exports = auth;
