3a8db1ac40a8f42174cff7f82f8929b4b3c4019e
Souvik273 <souvikgoswami464@gmail.com>
2025-01-24 16:27:15 +0530

    create note component created

---
+++ client/components/notes/CreateNote.jsx
@@ -0,0 +1,40 @@
+import axios from "axios"
+import { useState } from "react"
+
+const CreateNote = ()=>{
+
+    const [note,setNote] = useState({title:"",desc:""})
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log(note)
+        const accessToken = localStorage.getItem("accessToken")
+
+        axios.post("http://localhost:3000/notes/",
+            note,
+            {headers:{
+                "Authorization": `Bearer ${accessToken}`
+            }}
+            
+        )
+        .then((res)=>{
+            console.log(res.data)
+        })
+        .catch(err=>console.log(err.response.data))
+    }
+
+    return (
+        <>
+            <h2>Create Your note</h2>
+
+            <input type="text" placeholder="title" onChange={(e)=>setNote((prev)=>({...prev,title:e.target.value}))}/>
+
+            <textarea placeholder="Enter Your Note Here" onChange={(e)=>setNote((prev)=>({...prev,desc:e.target.value}))}></textarea>
+
+            <button onClick={handleSubmit}>Cretae Note</button>
+        </>
+    )
+}
+
+export { CreateNote }
+
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import { CreateNote } from "../components/notes/CreateNote"
 import { LogIn } from "../components/user/Login"
 import { SignUp } from "../components/user/SignUp"
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LogIn/>}/>
           <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/create-note" element={<CreateNote/>}/>
         </Routes>
     </>
   )
