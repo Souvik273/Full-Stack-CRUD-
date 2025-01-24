f7b1ac47e76879a57d066e9bd1e8a1e3a361ea2a
Souvik273 <souvikgoswami464@gmail.com>
2025-01-24 17:40:19 +0530

    dashboard created successfully

--- client/components/user/Dashboard.jsx
+++ client/components/user/Dashboard.jsx
@@ -1,12 +1,16 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from 'react-router'
+import { useNavigate } from 'react-router-dom'
+import { EditNotes } from "../notes/EditNotes"
 
 const Dashboard = ()=>{
+    const navigate = useNavigate()
     const [notes,setNotes] = useState([])
 
-    useEffect(()=>{
+    const accessToken = localStorage.getItem("accessToken")
 
-        const accessToken = localStorage.getItem("accessToken")
+    useEffect(()=>{
 
         axios.get("http://localhost:3000/notes/",{headers:{
             "Authorization": `Bearer ${accessToken}`
@@ -19,26 +23,35 @@ const Dashboard = ()=>{
     },[])
 
     const handleDelete=(noteId) =>{
-        
+        axios.delete(`http://localhost:3000/notes/${noteId}`,{headers:{
+            "Authorization": `Bearer ${accessToken}`
+        }})
+        .then(()=>{
+            console.log(`Note deleted successfully`)
+            navigate("/dashboard")
+    })
+        .catch(err=>console.log(err))
     }
 
     const handleEdit=(noteId) =>{
-
+        <EditNotes noteId={noteId}/>
     }
 
     return (
         <>
             <h2>Welcome to Dashboard !!!</h2>
+            
             {/* Render notes here */}
             <ul>
-                {notes.map(note => (
+                {notes.length !==0 ? notes.map(note => (
                     <li key={note._id}>
                     title:{note.title},
                     description:{note.desc}
                     <button onClick={()=>handleEdit(note._id)}>Edit</button>
                     <button onClick={()=>handleDelete(note._id)}>Delete</button>
                     </li>
-                ))}
+                )):"you don't have any notes...  "}
+                <Link to={"/create-note"}>create new Notes ? </Link>
             </ul>
         </>
     )
