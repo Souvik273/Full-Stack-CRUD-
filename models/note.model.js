11c21e910a35aaf4c30eebdb948654ce339ff9b1
Souvik273 <souvikgoswami464@gmail.com>
2025-01-24 13:14:54 +0530

    note update and delete functionality added

--- models/note.model.js
+++ models/note.model.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const noteSchema = new mongoose.Schema({
-    title:{type:String,required:true},
-    desc:{type:String,required:true},
-    userId:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
-    user:{type:String,required:true}
-},{versionKey:false,timestamps:true})
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    username: { type: String, required: true },
+  },
+  { versionKey: false, timestamps: true },
+);
 
-const Note = mongoose.model('Note',noteSchema)
-module.exports=Note
+module.exports = mongoose.model('Note', noteSchema); 
--- routes/note.route.js
+++ routes/note.route.js
@@ -1,35 +1,61 @@
-const express = require('express')
-const Note = require('../models/note.model.js')
+const express = require('express');
+const Note = require('../models/note.model.js');
 
-const Router = express.Router()
-const auth = require('../middleware/auth.middleware.js')
+const Router = express.Router();
+const auth = require('../middleware/auth.middleware.js');
 
-Router.post('/',auth,async(req,res)=>{
-    const {title,desc,userId,user} = req.body
-    try {
-        const newNote = new Note(req.body)
-        await newNote.save()
-        res.status(200).json(`Note Created successfully ${newNote}`)
-    } catch (error) {
-        res.status(500).json(`Internal Server Error : ${error.message}`)
+// create new note 
+Router.post('/', auth, async (req, res) => {
+  const { title, desc,userId , username } = req.body;
+
+  if (!title || !desc || !username || !userId) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  try {
+    const newNote = new Note(req.body);
+    await newNote.save();
+    res.status(200).json(`Note Created successfully ${newNote}`);
+  } catch (error) {
+    res.status(500).json(`Internal Server Error : ${error.message}`);
+  }
+});
+
+Router.get('/', auth, async (req, res) => {
+  try {
+    const notes = await Note.find({ userId: req.body.userId });
+    res.status(200).json(notes);
+  } catch (err) {}
+});
+
+Router.patch('/:noteId', auth, async(req, res) => {
+  const {noteId} = req.params
+  const note = await Note.findOne({_id:noteId})
+  try {
+    if(note.userId.toString() === req.body.userId){
+      await Note.findByIdAndUpdate({_id:noteId},req.body)
+      return res.status(200).json({msg:`Note Updated`})
+    }else{
+      return res.status(400).json({msg:`You are not authorized to perform this task`})
     }
-})
+  } catch (error) {
+    console.log(error)
+  }
+});
 
-Router.get('/',auth,async(req,res)=>{
-    try{
-        const notes = await Note.find({userId:req.body.userId})
-        res.status(200).json(notes)
-    }catch(err){
-        
+Router.delete('/:noteId', auth, async(req, res) => {
+  const {noteId} = req.params
+  const note = await Note.findOne({_id:noteId})
+  try {
+    if(note.userId.toString() === req.body.userId){
+      await Note.findByIdAndDelete({_id:noteId})
+      return res.status(200).json({msg:`Note Deleted`})
+    }else{
+      return res.status(400).json({msg:`You are not authorized to perform this task`})
     }
-})
+  } catch (error) {
+    console.log(error)
+  }
+});
 
-Router.patch('/:noteId',auth,(req,res)=>{
-
-})
-
-Router.delete('/:noteId',auth,(req,res)=>{
-    
-})
-
-module.exports = Router
+module.exports = Router;
