cacdf424dcc5749d921b745e80ca7d179db9a3b4
Souvik273 <souvikgoswami464@gmail.com>
2025-01-24 14:43:08 +0530

    Basic react application created using vite

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const UserRouter = require('./routes/user.route.js')
 const NoteRouter = require('./routes/note.route.js')
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json({limit:'16kb'}))
+app.use(cors({
+    origin:"http://localhost:5173",
+    optionsSuccessStatus:200
+}))
 
 app.use('/users',UserRouter)
 app.use('/notes',NoteRouter)
